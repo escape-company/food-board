cf8f13b0ce0fd7603a9a610ba03d3a13537e1825
soyun993 <soyun993@naver.com>
2020-03-19 01:11:05 +0900

    Add typeorm and type-graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [GraphQLModule.forRoot({})],
+  imports: [
+    GraphQLModule.forRoot({}),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'nodejsstudy.c1pqggqqcx7t.ap-northeast-2.rds.amazonaws.com',
+      port: 3306,
+      username: 'admin',
+      password: 'nodejsstudy1!',
+      database: '',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
