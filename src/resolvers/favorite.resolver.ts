030e16ceb952c3ad92b2cc31fba9ad46135bbbae
jhj209 <jhj2091@naver.com>
2020-04-15 13:06:15 +0900

    install class-transformer

--- src/resolvers/favorite.resolver.ts
+++ src/resolvers/favorite.resolver.ts
@@ -1,10 +1,10 @@
 import { Resolver, Query } from '@nestjs/graphql';
 import { FavoriteType } from '../types/favorites';
-import FavoriteService from '../services/favorites.service';
+import FavoriteService from '../services/favorite.service';
 
 @Resolver(_of => FavoriteType)
 export default class FavoriteResolver {
-  constructor(private readonly favoriteService: FavoriteService) {}
+  constructor(private readonly favoriteService: FavoriteService) { }
 
   @Query(_returns => [FavoriteType])
   async test() {
